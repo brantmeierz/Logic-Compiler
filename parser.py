2cb8487b7141985be2e77fd8c85439d5680bfd4f
Zach Brantmeier <brantmeierz@gmail.com>
2021-12-23 10:53:20 -0600

    Syntax replacement rules and basic tokenization

--- parser.py
+++ parser.py
@@ -39,6 +39,59 @@ class Operator(Token):
 # Replacement rules
 
 
-print(Sym.POSSIBILITY_DIAMOND)
+# Syntax replacements
+# |- to ⊢
+# -> to →
+syn_repl = [
+    ['|-', '⊢'],
+    ['|=', '⊨'],
+    ['->', '→'],
+    ['/\\', '∧'],
+    ['\\/', '∨'],
+    ['=>', '⇒'],
+    ['<=>', '⇔'],
+    ['<->', '⇔'],
+    ['_|_', '⊥'],
+    [' or ', '∨'],
+    [' and ', '∧'],
+    [' implies ', '→'],
+    ['not ', '¬'],
+    [' xor ', '⊕'],
+    #['for all ', '∀'],
+]
+def first_pass(input_string):
+    for rule in syn_repl:
+        input_string = input_string.replace(rule[0], rule[1])
+    return input_string
 
-print(Sym.NOT_WEDGE)
+def tokenize(input_string):
+    token_list = []
+    for i in input_string:
+        if i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            token_list.append({'type': 'VAR', 'val': i})
+        elif i in '(':
+            token_list.append({'type': 'LPAREN', 'val': i})
+        elif i in ')':
+            token_list.append({'type': 'RPAREN', 'val': i})
+        elif i in '+-∨∧→⇒⇔⊕⊻=&':
+            token_list.append({'type': 'BINOP', 'val': i})
+        elif i in '~∀∃∄¬':
+            token_list.append({'type': 'UNOP', 'val': i})
+        else:
+            token_list.append({"type": 'UNKN', 'val': i})
+    return token_list
+
+input_string = '(A or B) xor (A and B) implies C'
+post_replace = first_pass(input_string)
+token_list = tokenize(post_replace)
+
+print(input_string)
+print(post_replace)
+print(token_list)
+
+#parse_string = "<>"
+
+
+#print(Sym.POSSIBILITY_DIAMOND)
+
+#print(Sym.AND_WEDGE)
